9da4c5e18d9b47c1641bf957b36b490c7186a4ec
fabiano <91575675+fabianoctito@users.noreply.github.com>
2023-11-16 18:02:19 -0300

    Add files via upload

--- src/components/pagInicial/HomeScreen.jsx
+++ src/components/pagInicial/HomeScreen.jsx
@@ -1,27 +1,60 @@
 import { useEffect, useState } from "react";
-import { StyleSheet, Text, TouchableOpacity, View, TextInput, } from "react-native";
+import { StyleSheet, Text,  Image, TouchableOpacity, View, TextInput, ScrollView, FlatList, StatusBar } from "react-native";
 import Rank from "./Rank";
+
+
 
 export default function HomeScreen() {
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            {/* <Image></Image> */}
-
-            <Text>SEJA BEM VINDO AO BEST.FM</Text>
+        <ScrollView>
+            <View><Text>SEJA BEM VINDO AO BEST.FM</Text></View>
             <View className='tables'>
-                <View className='artTable'>
+                <View className='artTable' style={styles.musicaTop}>
                     <Text>TOP 10 ARTISTAS</Text>
-                    <Rank id='1'/>
+
+
                 </View>
 
-                <View className='musTable'>
+                <View className='musTable' style={styles.musicaTop}>
                     <Text>TOP 10 MÚSICAS</Text>
+                    <Rank id='2'/>
+
                 </View>
 
                 <View className='albTable'>
                     <Text>TOP 10 ÁLBUNS</Text>
                 </View>
             </View>
-        </View>
+        </ScrollView>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: StatusBar.currentHeight || 0,
+    },
+    item: {
+        backgroundColor: '#f9c2ff',
+        padding: 4,
+        paddingHorizontal: 50,
+        marginVertical: 4,
+        marginHorizontal: 8,
+        flexDirection: 'row', 
+        alignItems: 'center', 
+    },
+    title: {
+        fontSize: 19,
+    },
+
+    tinyLogo: {
+        width: 30,
+        height: 30,
+      },
+      musicaTop: {
+        alignItems: 'center',
+        backgroundColor: 'gray',
+        marginVertical: 4,
+        marginHorizontal: 16,
+      }
+});
--- src/components/pagInicial/Rank.jsx
+++ src/components/pagInicial/Rank.jsx
@@ -1,19 +1,115 @@
-import { StyleSheet, Text, TouchableOpacity, View, TextInput, } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View, TextInput, ScrollView, FlatList, StatusBar } from "react-native";
 import { consultarRank } from '../dados.jsx'
 // import ItemRank from "./itemRank.jsx";
 
 // const [itens, setItens] = useState([]);
 
+
+const DATA = [
+    {
+        musica: 'water',
+        artista: 'tahyla'
+    },
+    {
+        musica: 'urias',
+        artista: 'her mind'
+    },
+    {
+        musica: 'pipa',
+        artista: 'silvio santos'
+    },
+    {
+        musica: 'urias',
+        artista: 'her mind'
+    },
+    {
+        musica: 'urias',
+        artista: 'her mind'
+    },
+    {
+        musica: 'urias',
+        artista: 'her mind'
+    },
+    {
+        musica: 'urias',
+        artista: 'her mind'
+    },
+    {
+        musica: 'urias',
+        artista: 'her mind'
+    },
+    {
+        musica: 'urias',
+        artista: 'her mind'
+    },
+    {
+        musica: 'urias',
+        artista: 'her mind'
+    },
+
+]
+
 export default function Rank(props) {
-    if (props.id == 1) {
+
+    if (props.id == '2') {
+        const Item = (props) => (
+            <View style={styles.item}>
+                {/* <Image style={styles.tinyLogo} source={{uri: 'https://akamai.sscdn.co/letras/360x360/albuns/a/0/c/3/986991605733328.jpg'}}></Image> */}
+                <Text style={styles.title}>  {props.musica} - {props.artista}</Text>
+            </View>
+        );
+
+        async function pegarTracks() {
+            try {
+                const response = await fetch('https://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&format=json&api_key=32c7f2300cd26210d0ffcb714ce26ca7&limit=10');
+                const json = await response.json();
+                // console.log(json.tracks.track[0].name)
+                // console.log(json.tracks.track[0].artist.name)
+                
+                return json.tracks.track;
+            } catch (error) {
+                console.error(error);
+            }
+        }
+
+        //  const DATA = await pegarTracks()
+         
         return (
-            <View>
-                {/* {consultarRank(1).forEach(e => {
-                    // <Text>{e.name}</Text>    
-                    console.log(e)
-                })
-                } */}
-                {cons}
-            </View>)
+            <FlatList
+                data={DATA}
+                renderItem={({ item }) => <Item musica={item.name} artista={item.artist.name} />}
+                // keyExtractor={item => item.musica}
+            />
+        )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: StatusBar.currentHeight || 0,
+    },
+    item: {
+        backgroundColor: '#f9c2ff',
+        padding: 4,
+        paddingHorizontal: 50,
+        marginVertical: 4,
+        marginHorizontal: 8,
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: 19,
+    },
+
+    tinyLogo: {
+        width: 30,
+        height: 30,
+    },
+    musicaTop: {
+        alignItems: 'center',
+        backgroundColor: 'gray',
+        marginVertical: 4,
+        marginHorizontal: 16,
+    }
+});
