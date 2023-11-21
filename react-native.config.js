5c429e36b3c8ba3df6fc5ce628ade4c38ba6c890
itstuts <135055631+itstuts@users.noreply.github.com>
2023-11-21 11:58:06 -0300

    fonte

---
+++ react-native.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    project: {
+      ios: {},
+      android: {},
+    },
+    assets: ['./assets/fonts'],
+  }; 
+
+  // https://fonts.google.com/specimen/Ubuntu?preview.text=nawrfrt
