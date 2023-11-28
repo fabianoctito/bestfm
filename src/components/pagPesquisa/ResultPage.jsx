e8874f0e4c30ec930b527d31bf363d8bb06bae8b
fabiano <titoneli.fabiano@gmail.com>
2023-11-28 20:15:03 -0300

    mostrar resultado de artista da tela 2

--- src/components/pagPesquisa/ResultPage.jsx
+++ src/components/pagPesquisa/ResultPage.jsx
@@ -1,28 +1,38 @@
 import { StyleSheet, Text, TouchableOpacity, View, Alert, Platform, StatusBar, Image, ScrollView, FlatList, Pressable } from "react-native";
 import styles2 from '../Estilos.jsx';
+import { retornarInfo } from "../../utils.jsx";
+import { useState } from "react";
 
 export default function ResultPage({ route, navigation }) {
-    const { id, nome, artista, ouvintes, imagem } = route.params
+    const [resultInfo, setResultInfo] = useState({})
+
+    const { id, nome, artista } = route.params
     if (id == 1) {
         return (
             <View>
-                <Text>{nome} - {artista} - {ouvintes} ouvintes no last.fm</Text>
-                <Pressable style={styles.botao} onPress={() => navigation.pop()}><Text>Voltar</Text></Pressable>
+                {/* <Text>{nome} - {artista} - {ouvintes} ouvintes no last.fm</Text>
+                <Pressable style={styles.botao} onPress={() => navigation.pop()}><Text>Voltar</Text></Pressable> */}
             </View>
         )
     } else if (id == 2) {
+        const coletarRes = async () => retornarInfo(id,nome).then((res)=>setResultInfo(res))
+        coletarRes()
         return (
             <View>
-                <Text>{nome} - {ouvintes} ouvintes no last.fm</Text>
+                <Text>{nome}</Text>
+                <Text>No total, {resultInfo.plays} e {resultInfo.ouvintes} ouvintes no last.fm!</Text>
+                <Text>
+                    {resultInfo.bio ? resultInfo.bio: ''}
+                </Text>
                 <Pressable style={styles.botao} onPress={() => navigation.pop()}><Text>Voltar</Text></Pressable>
             </View>
         )
     } else if (id == 3) {
         return (
             <View>
-                <Text>{nome} - {artista}</Text>
+                {/* <Text>{nome} - {artista}</Text>
                 <Pressable style={styles.botao} onPress={() => navigation.pop()}><Text>Voltar</Text></Pressable>
-                <Image style={{width: 200, height: 200}} source={{uri: imagem['#text']}}></Image>
+                <Image style={{width: 200, height: 200}} source={{uri: imagem['#text']}}></Image> */}
             </View>
         )
     }
--- src/utils.jsx
+++ src/utils.jsx
@@ -23,18 +23,18 @@ export async function realizarPesquisa(cod, texto) {
     try {
       const response = await fetch(`https://ws.audioscrobbler.com/2.0/?method=track.search&track=${texto}&api_key=32c7f2300cd26210d0ffcb714ce26ca7&format=json&limit=10`);
       const json = await response.json();
-      if(!json.results) return []
-      return [json.results.trackmatches.track, 1];      
+      if (!json.results) return []
+      return [json.results.trackmatches.track, 1];
     } catch (error) {
       console.error(error);
     }
   }
-  else if(cod == 2){
+  else if (cod == 2) {
     try {
       const response = await fetch(`https://ws.audioscrobbler.com/2.0/?method=artist.search&artist=${texto}&api_key=32c7f2300cd26210d0ffcb714ce26ca7&format=json&limit=10`);
       const json = await response.json();
-      if(!json.results) return []
-      return[json.results.artistmatches.artist, 2];
+      if (!json.results) return []
+      return [json.results.artistmatches.artist, 2];
     } catch (error) {
       console.error(error);
     }
@@ -43,11 +43,39 @@ export async function realizarPesquisa(cod, texto) {
     try {
       const response = await fetch(`https://ws.audioscrobbler.com/2.0/?method=album.search&album=${texto}&api_key=32c7f2300cd26210d0ffcb714ce26ca7&format=json&limit=10`);
       const json = await response.json();
-      if(!json.results) return []
-      console.log([json.results.albummatches.album, 1]);      
-      return [json.results.albummatches.album, 3];      
+      if (!json.results) return []
+      console.log([json.results.albummatches.album, 1]);
+      return [json.results.albummatches.album, 3];
     } catch (error) {
       console.error(error);
     }
+  }
+}
+
+export async function retornarInfo(cod, nome, artistaRel) {
+  if (cod = 1) {
+
+  }
+  else if (cod == 2) {
+    try {
+      let bio, tour, estilos
+      const response = await fetch(`https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${nome}&api_key=32c7f2300cd26210d0ffcb714ce26ca7&format=json&lang=pt`);
+      const json = await response.json();
+      bio = json.artist.bio.published.includes('1970') ? null : json.artist.bio.summary
+      tour = json.artist.ontour ? true : false
+      estilos = json.artist.tags.tag.length == 0 ? null : json.artist.tags.tag
+      return ({
+        bio,
+        ouvintes: json.artist.stats.listeners,
+        plays: json.artist.stats.playcount,
+        tour,
+        estilos
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  else if (cod = 3) {
+
   }
 }
