5aca899d520bd30190a47badee4ea5c86f9a7995
Gabriel Oliveira <98922667+Gabssdesu@users.noreply.github.com>
2023-12-02 18:33:53 -0300

    adic icones

--- src/components/pagInicial/HomeScreen.jsx
+++ src/components/pagInicial/HomeScreen.jsx
@@ -9,13 +9,13 @@ export default function HomeScreen() {
             <View style={styles.cabecalho}>
                 <Image style={styles.logo} source={require('../img/logo.png')}></Image>
                 <Text style={styles.titulo}>SEJA BEM VINDO AO BEST.FM</Text>
+                
                 </View>
             <View className='tables'>
                 <View style={styles.musicaTop}>
                     <Text style={styles.titulo}>TOP 10 ARTISTAS GLOBAIS</Text>
                     <Rank id='1' />
                 </View>
-
                 <View style={styles.musicaTop}>
                     <Text style={styles.titulo}>TOP 10 MÚSICAS GLOBAIS</Text>
                     <Rank id='2' />
